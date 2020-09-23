c0f6efe355d515cb00553ac50bfe0bc08f3949e1
valuecodes <juhakangas5@gmail.com>
2020-09-23 13:53:22 +0300

    Finished task 15 details and added mobile responsiveness

--- 15_jobs_in_finland_task/js/script.js
+++ 15_jobs_in_finland_task/js/script.js
@@ -1,4 +1,9 @@
 function openSearch(){
-    console.log('open')
-    
+    let searchContainer = document.getElementById('searchContainer')
+    let state = searchContainer.style.visibility
+    if(state==='hidden'||state===''){
+        searchContainer.style.visibility='visible'
+    }else{
+        searchContainer.style.visibility='hidden'
+    }
 }
