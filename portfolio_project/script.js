d667fc536de76ff8c07d08c1941e6a8837ab0cb7
valuecodes <juhakangas5@gmail.com>
2020-10-07 12:35:58 +0300

    Added carousel

--- portfolio_project/script.js
+++ portfolio_project/script.js
@@ -1,4 +1,38 @@
-console.log()
+
+
+(function () {
+    let carousel = document.getElementById('carousel')
+    let numberOfPictures = 4
+    let imageWidth = 1200
+    let speed = 1;
+    let direction = 1
+    
+    setInterval(()=>{
+        let right = Number(carousel.style.right.split('px')[0])
+        let width = imageWidth*(numberOfPictures-1)
+        
+        right+= speed * direction
+
+        if(right > width){
+            direction=-1
+        }
+
+        if(right<0){
+            direction=1
+        }
+
+        carousel.style.right=right+'px'
+    }, 20);
+
+
+
+
+    
+})();
+
+
+
+
 
 function openMenu(){
     let navigation = document.getElementById('navigation')
